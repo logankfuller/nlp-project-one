4ec9b552ac6d6601da430224bdac49f22f09ea52
Devyn Caterer <dcaterer.git@gmail.com>
2017-10-20 21:02:43 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,214 +1,171 @@
 import os
 import nltk
 from nltk.tokenize import RegexpTokenizer
-from collections import defaultdict
-from itertools import *
-import copy
 import random
 import pickle
 import pprint
-from collections import OrderedDict
-
-trivialTokenizer = RegexpTokenizer(r"\d+|Mr\.|Mrs\.|Dr\.|\b[A-Z]\.|[a-zA-Z_]+-[a-zA-Z_]+-[a-zA-Z_]+|[a-zA-Z_]+-[a-zA-Z_]+|[a-zA-Z_]+|--|'s|'t|'d|'ll|'m|'re|'ve|[.,:!?;\"'()\[\]&@#-]")
-
-WORD_TYPE_COUNT = 12000
-
-# Create an initialized model where everything is 0
-model = {}
-
-def dictCopy(item):
-    temp = {}
-    newCopy = {}
-    temp = item
-    newCopy = temp
-    return newCopy
-
-def removeKey(mDict, key):
-    r = dict(mDict)
-    del r[key]
-    return r
-
-def weighted_choice(choices, prob):
-    r = random.uniform(0.0, prob)
-    upto = 0
-    for c in choices.items():
-        if upto + c[1] >= r:
-            return c[0]
-        upto += c[1]
 
 def file_input(userPath):
-   path = input(userPath)
-   if os.path.isdir(path):
-      return path
-   else:
-      return file_input('Enter an existing file directory: ')
+	path = input(userPath)
+	if os.path.isdir(path):
+		return path
+	else:
+		return file_input('Enter an existing file directory: ')
 
-# SANITY CHECK (THERE IS NO SANITY LEFT IN US)
 def getMaxProb(unigram):
-    probability = 0
-    for word in model[unigram].items():
-        probability += word[1]
-    return probability
+	probability = 0
+	for word in model[unigram].items():
+		probability += word[1]
+	return probability
 
 userEntry = 'N'
 if os.path.exists('model.pickle'):
-    userEntry = input("Use pickled dictionary? (Y/N)")
+	userEntry = input("Use pickled dictionary? (Y/N)")
 
 if userEntry == 'Y' or userEntry == 'y':
-    with open('model.pickle', 'rb') as handle:
-        model = pickle.load(handle)
+	with open('model.pickle', 'rb') as handle:
+		model = pickle.load(handle)
 else:
-    directory = file_input('Enter a directory: ')
+	directory = file_input('Enter a directory: ')
 
-    trainingData = []
+	trivialTokenizer = RegexpTokenizer(r"\d+|Mr\.|Mrs\.|Dr\.|\b[A-Z]\.|[a-zA-Z_]+-[a-zA-Z_]+-[a-zA-Z_]+|[a-zA-Z_]+-[a-zA-Z_]+|[a-zA-Z_]+|--|'s|'t|'d|'ll|'m|'re|'ve|[.,:!?;\"'()\[\]&@#-]")
 
-    for file in os.listdir(directory):
-        if file.endswith('.txt'):
-            fileData = open(directory + '\\' + file).read()
-            trainingData += fileData
-        else:
-            print('Found non-txt file: ' + file)
+	trainingData = []
 
-    trainingData = ''.join(trainingData)
+	for file in os.listdir(directory):
+		if file.endswith('.txt'):
+			fileData = open(directory + '\\' + file).read()
+			trainingData += fileData
+		else:
+			print('Found non-txt file: ' + file)
 
-    # Begin tokenizing
-    trainingData = trivialTokenizer.tokenize(trainingData)
+	trainingData = ''.join(trainingData)
 
-    unigramCount = len(trainingData)
+	# Begin tokenizing
+	trainingTokens = trivialTokenizer.tokenize(trainingData)
 
-    freqDist = nltk.FreqDist(trainingData)
-    commonFreqDist = nltk.FreqDist(trainingData).most_common(WORD_TYPE_COUNT)
+	WORD_TYPE_COUNT = 4000
 
-    bigrams = nltk.bigrams(trainingData)
+	unigrams = nltk.FreqDist(trainingTokens)
+	bigrams = nltk.bigrams(trainingTokens)
+	commonFreqDist = nltk.FreqDist(trainingTokens).most_common(WORD_TYPE_COUNT)
+	bigramFreqDist = nltk.FreqDist(nltk.bigrams(trainingTokens))
 
-    # For each bigram, we sort by the first word
-    vWordTypes = []
-    sortedBigramDict = {}
-    for x in commonFreqDist:
-        vWordTypes.append(x[0])
+	 # Create an initialized model where everything is 0
+	model = {}
+	
+	vWordTypes = []
+	for x in commonFreqDist:
+		vWordTypes.append(x[0])
 
-    for word in bigrams:
-        x = False
-        y = False
-        if word[0] in vWordTypes:
-            x = True 
-        if word[1] in vWordTypes:
-            y = True
+	# Add each frequent word into a model with a None value
+	# freqWord[0] = word, [1] = count
+	for freqWord in commonFreqDist:
+		rowToAdd = {}
+		# Add a copy of the model to each word type already in the model (this creates an identical 2d array)
+		for tempWord in commonFreqDist:
+			rowToAdd[tempWord[0]] = 0
+		model[freqWord[0]] = rowToAdd
+	
+	# Clean up the bigram freq dist 
+	'''
+	cleanedBigramFreqDist = []
+	for bigram in bigramFreqDist.items():
+		if(bigram[0][0] in commonFreqDist and bigram[0][1] in commonFreqDist):
+	'''	
+	
+	cleanedBigramFreqDist = []
+	for bigram in bigramFreqDist.items():
+		x = False
+		y = False
+		if bigram[0][0] in vWordTypes:
+			x = True 
+		if bigram[0][1] in vWordTypes:
+			y = True
 
-        tempList = []
+		if x == True and y == True:
+			cleanedBigramFreqDist.append(bigram)
 
-        if x == True and y == True:
-            if word[0] in sortedBigramDict:
-                tempList = sortedBigramDict[word[0]]
-            tempList.append(word[1])
-            sortedBigramDict[word[0]] = tempList
+	#print(cleanedBigramFreqDist)
+	
+	'''
+	unigramList = []
+	for item in cleanedBigramFreqDist:
+		unigramList.append(item[0][0])
+		#unigramList.append(item[0][1])
+	
+	unigrams = nltk.FreqDist(unigramList)
+	'''
+	
+	#print(unigrams.items())
 
-    # print(sortedBigramDict) 'leisurely': ['walk', 'turns', 'turns', 'blah', 'etc'], 
+	# How to access cleanedBigramFreqDist
+	# print(cleanedBigramFreqDist[0][0][0])
 
-    # Creating an empty slate
-    vTypeDict = {}
-    for v in vWordTypes:
-        vTypeDict[v] = 0
+	# Now to populate the inner values with actual bigram data
+	# bigram[0] returns first word, bigram[1] returns second word, value returns bigram count
+	
+	for bigram, value in cleanedBigramFreqDist:
+		model[bigram[0]][bigram[1]] = value
+		
+	sums = {}
+	for outer in model:
+		s = 0
+		for inner in model[outer]:
+			s += model[outer][inner]
+		sums[outer] = s
+	
+	for s in sums:
+		sums[s] /= unigrams[s]
+	
+	
+	#print(sums)
 
-    for v in vWordTypes:
-        # Copy temporary dictionary
-        tempDict = {}
-        tempDict = dictCopy(vTypeDict)
+	# ------------ Smoothing ------------- #
+	# Smooth bigram (a,b) + i
+	# Smooth Unigram count (a) + (V * i)
+	# Divide those two for probabilities
+	# probability r* = (cnt(a,b) + i) / ((a) + (V * i))
+	
+	# {'test': {'test1': 0}}
+	# outer = 'test', inner = 'test1'
+	
+	# Get list of bigrams with counts 1-9
+	lowFreqBigramCount = 0
+	for outer in model:
+		for inner in model[outer]:
+			value = 0
+			value = model[outer][inner]
+			if value < 10 and value > 0:
+				lowFreqBigramCount += 1
+	
+	# Increase each bigram count by 1/lowFreqBigramCount
+	additiveBigram = 0
+	additiveBigram = 1/lowFreqBigramCount
+	for outer in model:
+		for inner in model[outer]:
+			model[outer][inner] += additiveBigram
 
-        # Copy sorted bigram list
-        if v in sortedBigramDict:
-            tempList = []
-            tempList = dictCopy(sortedBigramDict[v])
-        for x in tempList:
-            if x != v:
-                tempDict[x] += 1
+	print(model['the'])
+	
+	# Increase each unigram count by WORD_TYPE_COUNT/lowFreqBigramCount
+	additiveUnigram = 0
+	additiveUnigram = WORD_TYPE_COUNT * additiveBigram
+	for unigram in unigrams:
+		unigrams[unigram] += additiveUnigram
 
-        # ------------ Smoothing
-        
-        # Get list of bigrams with counts 1-9
-        lowFreqBigramCount = 0
-        for bigram in tempDict.items():
-            if bigram[1] <= 9 and bigram[1] > 0:
-                lowFreqBigramCount += 1
+	#print(sum(model['asks'].values()))
+	
+	# Calculate the probabilities of each bigram count
+	for outer in model:
+		for inner in model[outer]:
+			#print(str(model[outer][inner]) + ' : ' + str(unigrams[outer]))
+			model[outer][inner] /= unigrams[outer]
+			
+for outer in model:
+	print(outer + ':' + str(getMaxProb(outer)))
 
-        # Increase each bigram count by 1/lowFreqBigramCount
-        additiveBigram = 0
-        if lowFreqBigramCount != 0:
-            additiveBigram = 1/lowFreqBigramCount
-            for bigram in tempDict.items():
-                tempDict[bigram[0]] += additiveBigram
-
-        # Increase each unigram count by V/lowFreqBigramCount
-        currentUnigramCount = 0
-        additiveUnigram = 0
-        if lowFreqBigramCount != 0:
-            additiveUnigram = WORD_TYPE_COUNT / lowFreqBigramCount
-            currentUnigramCount = freqDist[v] + additiveUnigram
-            
-        # Probabilities
-        for bigram in tempDict.items():
-            if currentUnigramCount != 0:
-                tempDict[bigram[0]] /= currentUnigramCount
-
-        model[v] = tempDict
-
-    with open('model.pickle', 'wb') as handle:
-        pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-#pprint.pprint(model['these'])
-    #print(model['plot'])
-
-# Sentence generation
-print("Generating sentence...")
-
-# Pick a random word
-sentence = []
-
-# Grab random first word and append
-currentWord = random.choice(list(model.keys()))
-sentence.append(currentWord)
-
-print('Initial Rand: ', currentWord)
-
-sentence_finished = False
-while not sentence_finished:
-    r = random.random()
-    accumulator = .0
-
-    #orderedModel = OrderedDict(sorted(model[currentWord].items(), key=lambda kv: kv[1], reverse=True))
-
-    for word in model[currentWord]:
-        accumulator += model[currentWord][word]
-
-        if accumulator >= r:
-            if word != currentWord:
-                sentence.append(word)
-                currentWord = word
-                break
-
-    if(len(sentence) >= 30):
-        sentence_finished = True
-
-print(' '.join(sentence))
-
-print(sentence)
-sentenceBigrams = nltk.bigrams(sentence)
-
-sentenceProbability = 1.0
-for bigram in sentenceBigrams:
-    print(model[bigram[0]][bigram[1]])
-    sentenceProbability *= model[bigram[0]][bigram[1]]
-
-print(sentenceProbability)
-
-
-
-
-
-
-
-
-# Calculating probability of sentences
-
-sentenceOne = "Go to the park and meet girls"
-sentenceTwo = "No, I'm not paying for the damage to your car"
+'''
+	#with open('model.pickle', 'wb') as handle:
+	#	pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+'''
