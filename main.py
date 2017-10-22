f527d81d9abf236979f3f6f3ca8b688ed6502b1e
Devyn Caterer <dcaterer.git@gmail.com>
2017-10-21 17:41:44 -0700

    Functional Main
    Working and functional. Only generate above 4000 if using 64bit python

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
+import os, re
 import nltk
 from nltk.tokenize import RegexpTokenizer
+from nltk import word_tokenize
 import random
 import pickle
 import pprint
@@ -17,18 +18,95 @@ def getMaxProb(unigram):
 	for word in model[unigram].items():
 		probability += word[1]
 	return probability
+	
+def sentenceProbability(s):
+	sBigrams = nltk.bigrams(s)
+	sProb = 1.0
+	
+	for bigram in sBigrams:
+		sProb *= model[bigram[0]][bigram[1]]
+	print('> Sentence Probability: ' + str(sProb))
+	
+def compareSentences(s1, s2):
+	s1Bigrams = nltk.bigrams(trivialTokenizer.tokenize(s1))
+	s2Bigrams = nltk.bigrams(trivialTokenizer.tokenize(s2))
+	s1prob = 1.0
+	s2prob = 1.0
+	
+	for bigram in s1Bigrams:
+		s1prob *= model[bigram[0]][bigram[1]]
+	
+	for bigram in s2Bigrams:
+		s2prob *= model[bigram[0]][bigram[1]]
+		
+	if s1prob > s2prob:
+		print('> S1 Probability: ' + str(s1prob) + '| S2 Probability: ' + str(s2prob))
+		print('> Sentence 1 has higher probability')
+	else:
+		print('> S1 Probability: ' + str(s1prob) + '| S2 Probability: ' + str(s2prob))
+		print('> Sentence 2 has higher probability')
 
+def generateSentence():
+	# Sentence generation
+	print('> ----- Sentence Generation -----')
+	cont = 'Y'
+	while cont == 'Y' or cont == 'y':
+		size = int(input('> Input Sentence Size [Integer]: '))
+	
+		print("> Generating sentence...")
+
+		# Pick a random word
+		sentence = []
+		qSentence = ''
+
+		# Grab random first word and append
+		currentWord = random.choice(list(model.keys()))
+		sentence.append(currentWord)
+
+		sentence_finished = False
+		while not sentence_finished:
+			r = random.uniform(0, 1.0)
+			accumulator = .0
+
+			for word in model[currentWord]:
+				accumulator += model[currentWord][word]
+
+				if accumulator >= r:
+					if word != currentWord:
+						sentence.append(word)
+						currentWord = word
+						break
+
+			if(len(sentence) >= size):
+				sentence_finished = True
+		
+		gSentence = ' '.join(sentence)
+		
+		print('> Generated sentence: ' + ' '.join(sentence))
+		cont = input('> Generate Again [Y/N]: ')
+	
+def doCompare():
+	print('> ----- Sentence Comparison -----')
+	cont = 'Y'
+	while cont == 'Y' or cont == 'y':
+		sent1 = input('> Input Sentence 1: ')
+		sent2 = input('> Input Sentence 2: ')
+		compareSentences(sent1, sent2)
+		cont = input('> Compare Again [Y/N]: ')
+
+ # Create an initialized model where everything is 0
+model = {}
+trivialTokenizer = RegexpTokenizer(r"\d+|Mr\.|Mrs\.|Dr\.|\b[A-Z]\.|[a-zA-Z_]+-[a-zA-Z_]+-[a-zA-Z_]+|[a-zA-Z_]+-[a-zA-Z_]+|[a-zA-Z_]+|--|'s|'t|'d|'ll|'m|'re|'ve|[.,:!?;\"'()\[\]&@#-]")
+	
 userEntry = 'N'
 if os.path.exists('model.pickle'):
-	userEntry = input("Use pickled dictionary? (Y/N)")
+	userEntry = input("> Use pickled dictionary? (Y/N)")
 
 if userEntry == 'Y' or userEntry == 'y':
 	with open('model.pickle', 'rb') as handle:
 		model = pickle.load(handle)
 else:
-	directory = file_input('Enter a directory: ')
-
-	trivialTokenizer = RegexpTokenizer(r"\d+|Mr\.|Mrs\.|Dr\.|\b[A-Z]\.|[a-zA-Z_]+-[a-zA-Z_]+-[a-zA-Z_]+|[a-zA-Z_]+-[a-zA-Z_]+|[a-zA-Z_]+|--|'s|'t|'d|'ll|'m|'re|'ve|[.,:!?;\"'()\[\]&@#-]")
+	directory = file_input('> Enter a directory: ')
 
 	trainingData = []
 
@@ -43,16 +121,14 @@ else:
 
 	# Begin tokenizing
 	trainingTokens = trivialTokenizer.tokenize(trainingData)
+	#trainingTokens = nltk.word_tokenize(trainingData)
 
-	WORD_TYPE_COUNT = 4000
+	WORD_TYPE_COUNT = 8000
 
 	unigrams = nltk.FreqDist(trainingTokens)
 	bigrams = nltk.bigrams(trainingTokens)
 	commonFreqDist = nltk.FreqDist(trainingTokens).most_common(WORD_TYPE_COUNT)
 	bigramFreqDist = nltk.FreqDist(nltk.bigrams(trainingTokens))
-
-	 # Create an initialized model where everything is 0
-	model = {}
 	
 	vWordTypes = []
 	for x in commonFreqDist:
@@ -68,13 +144,9 @@ else:
 		model[freqWord[0]] = rowToAdd
 	
 	# Clean up the bigram freq dist 
-	'''
-	cleanedBigramFreqDist = []
-	for bigram in bigramFreqDist.items():
-		if(bigram[0][0] in commonFreqDist and bigram[0][1] in commonFreqDist):
-	'''	
 	
 	cleanedBigramFreqDist = []
+	unigramCorrector = {}
 	for bigram in bigramFreqDist.items():
 		x = False
 		y = False
@@ -85,18 +157,15 @@ else:
 
 		if x == True and y == True:
 			cleanedBigramFreqDist.append(bigram)
-
+		elif x == True and y == False:
+			if bigram[0][0] in unigramCorrector:
+				unigramCorrector[bigram[0][0]] += 1
+			else:
+				unigramCorrector[bigram[0][0]] = 1
+		
 	#print(cleanedBigramFreqDist)
-	
-	'''
-	unigramList = []
-	for item in cleanedBigramFreqDist:
-		unigramList.append(item[0][0])
-		#unigramList.append(item[0][1])
-	
-	unigrams = nltk.FreqDist(unigramList)
-	'''
-	
+	#print(str(unigrams['M.']) + ':' + str(unigramCorrector['M.']))
+
 	#print(unigrams.items())
 
 	# How to access cleanedBigramFreqDist
@@ -114,11 +183,11 @@ else:
 		for inner in model[outer]:
 			s += model[outer][inner]
 		sums[outer] = s
-	
+	#print(str(sums['M.']))
 	for s in sums:
 		sums[s] /= unigrams[s]
 	
-	
+	#print(str(sums['M.']))
 	#print(sums)
 
 	# ------------ Smoothing ------------- #
@@ -145,14 +214,14 @@ else:
 	for outer in model:
 		for inner in model[outer]:
 			model[outer][inner] += additiveBigram
-
-	print(model['the'])
 	
 	# Increase each unigram count by WORD_TYPE_COUNT/lowFreqBigramCount
 	additiveUnigram = 0
 	additiveUnigram = WORD_TYPE_COUNT * additiveBigram
 	for unigram in unigrams:
 		unigrams[unigram] += additiveUnigram
+		if unigram in unigramCorrector:
+			unigrams[unigram] -= unigramCorrector[unigram]
 
 	#print(sum(model['asks'].values()))
 	
@@ -161,11 +230,10 @@ else:
 		for inner in model[outer]:
 			#print(str(model[outer][inner]) + ' : ' + str(unigrams[outer]))
 			model[outer][inner] /= unigrams[outer]
-			
-for outer in model:
-	print(outer + ':' + str(getMaxProb(outer)))
+	
+	with open('model.pickle', 'wb') as handle:
+		pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-'''
-	#with open('model.pickle', 'wb') as handle:
-	#	pickle.dump(model, handle, protocol=pickle.HIGHEST_PROTOCOL)
-'''
+generateSentence()
+doCompare()
+
